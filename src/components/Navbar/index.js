c3ec0c87005378cb0ad5c57593ea6b7e9b1c8dd3
D.L. White - Vunte Ron <dlwhite112@gmail.com>
2021-06-13 12:30:59 -0500

    nav images

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./style.css"
+import Icon from "../../assets/dl_dev_icon.png"
+import Create from "../../assets/we-create.png"
+import Contact from "../../assets/contact-me.png"
+
 export default function Navbar() {
   return (
     <div>
@@ -7,7 +11,7 @@ export default function Navbar() {
         <div className="logo-container">
           <div className="logo-link">
             <a href="#about-me">
-              <img src="../../../assets/dl_dev_icon.png" alt="dl.dev-logo" />
+              <img src={Icon} alt="dl.dev-logo" />
             </a>
           </div>
         </div>
@@ -15,14 +19,14 @@ export default function Navbar() {
           <div className="nav-container">
             <div className="nav-link">
               <a href="portfolio.html">
-                <img src="../../../assets/we-create.png" alt="portfolio-logo" />
+                <img src={Create} alt="portfolio-logo" />
               </a>
             </div>
           </div>
           <div className="contact-container">
             <div className="contact-link">
               <a href="contact.html">
-                <img src="../../../assets/contact-me.png" alt="contact-logo" />
+                <img src={Contact} alt="contact-logo" />
               </a>
             </div>
           </div>
